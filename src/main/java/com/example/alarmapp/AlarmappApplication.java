545bef8c4fee6f55d5d49ab70c1f69f94e5ab7e9
6zeropk <nemm3623@naver.com>
2025-12-03 13:44:52 +0900

    chore : 프로젝트 초기 설정

---
+++ src/main/java/com/example/alarmapp/AlarmappApplication.java
@@ -0,0 +1,13 @@
+package com.example.alarmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alarmapp/AlarmappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alarmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
