e0f4703e4c6873391b4502694623e2c1b85478bd
6zeropk <78200180+nemm3623@users.noreply.github.com>
2025-12-03 16:35:38 +0900

    Feat/1 member (#2)
    * feat : Member Entity, Repo, Dto 구현
    
    * feat : 회원가입 로직 구현
    
    * feat : 로그인 로직 구현

---
+++ src/main/java/com/example/alarmapp/member/dto/res/MemberJoinResDTO.java
@@ -0,0 +1,9 @@
+package com.example.alarmapp.member.dto.res;
+
+import lombok.Builder;
+
+@Builder
+public record MemberJoinResDTO(
+        long id
+) {
+}
---
+++ src/main/java/com/example/alarmapp/member/service/MemberCommandServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.alarmapp.member.service;
+
+
+import com.example.alarmapp.member.domain.Member;
+import com.example.alarmapp.member.dto.req.MemberJoinReqDTO;
+import com.example.alarmapp.member.dto.res.MemberJoinResDTO;
+import com.example.alarmapp.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberCommandServiceImpl implements MemberCommandService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberJoinResDTO registerMember(MemberJoinReqDTO dto){
+
+        if (memberRepository.existsByEmail(dto.email()))
+            throw new RuntimeException("Email already exists");
+
+        Member member = Member.builder()
+                .email(dto.email())
+                .password(dto.password())
+                .build();
+
+        memberRepository.save(member);
+
+        MemberJoinResDTO joinResDTO = MemberJoinResDTO.builder()
+                .id(member.getId())
+                .build();
+
+        return joinResDTO;
+    };
+}
